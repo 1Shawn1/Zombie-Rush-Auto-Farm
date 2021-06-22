946ecedb1fce9fcf9a42fdd89c5da0eff6dcbdea
Shawn <43222123+Fluxxx222@users.noreply.github.com>
2021-06-22 02:25:01 -0500

    Update Zombie Rush Auto Farm.lua

--- Zombie Rush Auto Farm.lua
+++ Zombie Rush Auto Farm.lua
@@ -6,32 +6,40 @@ StandOn.Size = Vector3.new(100, 1, 100)
 StandOn.CFrame = CFrame.new(39.6000061, 160.2000008, 56.2001953)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(39.6000061, 165.2000008, 56.2001953)
 local ZS = game:GetService("Workspace")["Zombie Storage"]
-ZS.ChildAdded:Connect(function(zombie)
-	if getgenv().On then
-		repeat
-			wait(0.1)
-			local KillThem = {
-				["Tool"] = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool"),
-				["RealTool"] = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool"),
-				["HumanoidTables"] = {
-					[1] = {
-						["HeadHits"] = 1,
-						["THumanoid"] = zombie:WaitForChild("Humanoid"),
-						["BodyHits"] = 0
+local IsGay = false
+if game.Players.LocalPlayer.UserId == 647004204 then
+	IsGay = true
+end
+if IsGay == false then
+	ZS.ChildAdded:Connect(function(zombie)
+		if getgenv().On then
+			repeat
+				wait(0.1)
+				local KillThem = {
+					["Tool"] = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool"),
+					["RealTool"] = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool"),
+					["HumanoidTables"] = {
+						[1] = {
+							["HeadHits"] = 1,
+							["THumanoid"] = zombie:WaitForChild("Humanoid"),
+							["BodyHits"] = 0
+						}
 					}
 				}
-			}
-			game:GetService("ReplicatedStorage").Remotes.WeaponEvent:FireServer(KillThem)
-		until zombie.Humanoid.Health == 0 or zombie == nil -- Repeats until zombies dead
-	end
-end)
-game:GetService("ReplicatedStorage").GameProperties.CurrentWave.Changed:Connect(function()
-	if game:GetService("ReplicatedStorage").GameProperties.CurrentWave.Value >= 900 then 
-		game.Players.LocalPlayer.Character.Humanoid.Health = 0 -- you die :(
-	end
-end)
-game.StarterGui:SetCore("SendNotification", {
-	Title = "Zombie Rush Auto Farm";
-	Text = "Made by: Shawn#2000";
-	Duration = 7;
-})
+				game:GetService("ReplicatedStorage").Remotes.WeaponEvent:FireServer(KillThem)
+			until zombie.Humanoid.Health == 0 or zombie == nil -- Repeats until zombies dead
+		end
+	end)
+	game:GetService("ReplicatedStorage").GameProperties.CurrentWave.Changed:Connect(function()
+		if game:GetService("ReplicatedStorage").GameProperties.CurrentWave.Value >= 900 then 
+			game.Players.LocalPlayer.Character.Humanoid.Health = 0 -- you die :(
+		end
+	end)
+	game.StarterGui:SetCore("SendNotification", {
+		Title = "Zombie Rush Auto Farm";
+		Text = "Made by: Shawn#2000";
+		Duration = 7;
+	})
+else
+	game.Players.LocalPlayer:Kick("GAY DETECTED")
+end
